3cfc8e279fb691e6a1f405ea011cf6376f3c18b7
arni <arni@sprettur.is>
2013-10-31 09:05:44 +0000

    Test for two strings

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,16 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		else if(text.contains(",")){
+			String[] numbers = text.split(",");
+			return toInt(numbers[0]) + toInt(numbers[1]);
+		}
 		else
 			return 1;
 	} 
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
      
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,6 +13,9 @@ public class CalculatorTest {
 	@Test
 	public void testOneString(){
 		assertEquals(1, Calculator.add("1"));
-
+	}
+	@Test
+	public void testTwoStrings(){
+		assertEquals(3, Calculator.add("1,2"));
 	}
 }
